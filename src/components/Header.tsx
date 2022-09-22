cdd8d496f53a49979044812a7cbbf4324fed89e3
Havrilenko Olexiy <hristinson@gmail.com>
2022-09-22 13:44:49 +0300

    React test

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import  {createElement as cElement} from 'react';
+
+
+function Header() {
+  const elem1 = cElement(`div`, {}, `DIVIVIDK`)
+  return (
+  
+        <h1> HEADER  {elem1} </h1> 
+  
+    );
+}
+
+export default Header;
