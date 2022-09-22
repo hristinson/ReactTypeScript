f5a76731e4148327bb75266a9316cd523e4bb340
Havrilenko Olexiy <hristinson@gmail.com>
2022-09-22 14:03:15 +0300

    useState added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,15 @@
-import  {createElement as cElement} from 'react';
+import  {useState, createElement as cElement} from 'react';
 
 
 function Header() {
-  const elem1 = cElement(`div`, {}, `DIVIVIDK`)
+    const [counter, setCounter] = useState(0)
+  const elem1 = cElement(`button`, {
+    onClick: () => setCounter(counter+1),
+    key: 23
+            }, `Click ME ${counter}`)
   return (
   
-        <h1> HEADER  {elem1} </h1> 
+        <h1> {counter} HEADER  {elem1} </h1> 
   
     );
 }
